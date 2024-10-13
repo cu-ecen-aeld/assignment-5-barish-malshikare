e49913d610e688e0340985d0afe55375a1dafec7
Barish Malshikare <malshikare.barish@gmail.com>
2024-10-13 21:51:52 +0530

    updating latest commit of asignment 3 and later

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '15de4780624c665664962b9921dd1d563d5baebe'
+AESD_ASSIGNMENTS_VERSION = 'bb5b2a00df5a43217a76f4c37a8419d030b3d3ce'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
